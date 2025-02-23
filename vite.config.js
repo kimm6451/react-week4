bb304a48e4682dfce3ebc9be892724b2dfe12c40
kimm6451 <wo30645@gmail.com>
2025-02-23 14:53:01 +0800

    變更路徑

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // 開發中、產品路徑
-	base: process.env.NODE_ENV === 'production' ? '/react-week4a/' : '/',
+	base: process.env.NODE_ENV === 'production' ? '/react-week4/' : '/',
 	plugins: [react()],
 })
